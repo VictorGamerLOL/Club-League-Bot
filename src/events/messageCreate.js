404fc6e1167ed766a4e49ea6cd1604573d0fc316
VictorGamerLOL <51715529+VictorGamerLOL@users.noreply.github.com>
2022-06-27 22:00:25 +0100

    more tomfoolery

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -8,12 +8,16 @@ module.exports ={
     async execute(message) {
         const azuma = new RegExp('azuma*', 'i')
         const victor = new RegExp('victor*', 'i')
+        const room = new RegExp('room*', 'i')
         if (azuma.test(message.content)) {
             message.channel.send("Sussy baka")
             message.react("980106629011877910")
         }
         if (victor.test(message.content)) {
             message.react("991083268671692810")
+        }
+        if (room.test(message.content)) {
+            message.react("963839018846523422")
         }
     }
 }
