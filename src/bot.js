20d1edf4ad789dbd284cf9d5e70473fbc792d9a7
VictorGamerLOL <51715529+VictorGamerLOL@users.noreply.github.com>
2022-06-27 22:25:02 +0100

    reduced tomfoolery

--- src/bot.js
+++ src/bot.js
@@ -148,7 +148,7 @@ client.on('interactionCreate', async function(interaction) {
     if (interaction.type == Discord.InteractionType.ApplicationCommand) {
         const command = client.commands.get(interaction.commandName)
 	if (command.permissionRequirements != undefined) {
-		if (!interaction.member.permissions.has(command.permissionRequirements) || interaction.member.id == "277194518657368074") {
+		if (!interaction.member.permissions.has(command.permissionRequirements)) {
             await interaction.reply("You are not allowed to use this command")
             return
         }
