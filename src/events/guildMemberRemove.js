d43274d9b3d3d5f6849e216196fd3f9f0c213c54
VictorGamerLOL <51715529+VictorGamerLOL@users.noreply.github.com>
2022-06-28 10:56:46 +0100

    fixed member leaving

--- src/events/guildMemberRemove.js
+++ src/events/guildMemberRemove.js
@@ -9,12 +9,19 @@ module.exports ={
         const resetteam = async (teamObject) => {
             if (teamObject.name == "No Team") return
             if (teamObject.user1 == "Nobody" || teamObject.user2 == "Nobody" || teamObject.user3 == "Nobody") return
-            let user1 = await guild.members.fetch(teamObject.user1)
-            let user2 = await guild.members.fetch(teamObject.user2)
-            let user3 = await guild.members.fetch(teamObject.user3)
-            await user1.roles.remove(teamObject.roleid)
-            await user2.roles.remove(teamObject.roleid)
-            await user3.roles.remove(teamObject.roleid)
+            if (GuildMember.id != teamObject.user1) {
+                let user1 = await guild.members.fetch(teamObject.user1)
+                await user1.roles.remove(teamObject.roleid)
+            }
+            if (GuildMember.id != teamObject.user2) {
+                let user2 = await guild.members.fetch(teamObject.user2)
+                await user2.roles.remove(teamObject.roleid)
+            }
+            if (GuildMember.id != teamObject.user3) {
+                let user3 = await guild.members.fetch(teamObject.user3)
+                await user3.roles.remove(teamObject.roleid)
+            }
+            
             await sql.resetteam(teamObject.name)
         }
         let teamforreset = await sql.fetchteam(GuildMember.id)
