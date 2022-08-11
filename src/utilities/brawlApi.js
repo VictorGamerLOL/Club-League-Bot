f334cd0cc85502609c72f3ce3aa431fde2be8cbd
VictorGamerLOL <nicetry@gmail.com>
2022-08-11 21:15:18 +0300

    Added brawl stars api

---
+++ src/utilities/brawlApi.js
@@ -0,0 +1,26 @@
+const token = require('../../config.json').brawlToken;
+const baseUrl = 'https://api.brawlstars.com/v1/';
+const clubId = ("%23" + require('../../config.json').clubId)
+
+const options = {
+  method: 'GET',
+  headers: {
+    Authorization: `Bearer ${token}`,
+  }
+};
+
+const afetch = (request) => {  //Asyncronous fetch because I need async await.
+  const url = baseUrl + request;
+  return new Promise(function (resolve, reject) {
+    fetch(url,options)
+      .then(response => resolve(response.json()))
+      .catch(error => reject(error));
+  })
+}
+
+module.exports = {
+  getClubMembers: async () => {
+    const response = await afetch(`clubs/${clubId}/members`);
+    return response.items;
+  }
+}
